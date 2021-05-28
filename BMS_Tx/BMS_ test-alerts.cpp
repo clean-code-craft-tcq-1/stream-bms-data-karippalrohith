7b44cecf45b145a27a64ba1e2586087d559c2262
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-05-28 17:56:21 +0530

    Create BMS_ test-alerts.cpp

---
+++ BMS_Tx/BMS_ test-alerts.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "BatteryMonitoringSystem_Main.h"
+
+TEST_CASE("Check if value is successfully send to cnsole for printing") {
+	
+	BMS_Status_s Alert_Status_Output = {NO_PRINT_DONE,DEFAULT_BREACH};
+	
+	//Alert_Status_Output = checkAndAlert(TO_CONTROLLER, batteryCharacter, 20);
+	//REQUIRE(Alert_Status_Output.Alert_Print_Status == PRINTED_IN_CONTROLLER);
+	//REQUIRE(Alert_Status_Output.breachType == NORMAL);
+}
